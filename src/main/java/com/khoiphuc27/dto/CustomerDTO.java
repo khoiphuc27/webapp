ca1c38a3d5e307a91f8520c1008ce57dd6a16646
Khoi Phuc Nguyen <nguyenkhoiphuc@gmail.com>
2018-10-10 18:29:20 +0700

    Add features

---
+++ src/main/java/com/khoiphuc27/dto/CustomerDTO.java
@@ -0,0 +1,67 @@
+package com.khoiphuc27.dto;
+
+public class CustomerDTO {
+	private String name;
+	private String birthday;
+	private String phone;
+	private String email;
+	private String gender;
+	private String address;
+	private String title;
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
